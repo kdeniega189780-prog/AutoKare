35c83e368006d301b1ef21883c847ddbc3a4cecc
Stryxen <camyranriegelicame@gmail.com>
2026-05-20 23:01:40 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import os from 'node:os';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    cacheDir: path.join(os.tmpdir(), 'vite-cache'),
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
